ff25ca7a0151c6869745012fb500cb48b7a21d73
Sebastian <s.m.prisacariu@gmail.com>
2024-07-17 21:12:34 +0200

    projects mobile

--- src/app/project.tsx
+++ src/app/project.tsx
@@ -24,7 +24,7 @@ export type Project = {
 
 export function Project(props: Project) {
   return (
-    <article className="border-b-foreground grid grid-cols-2 border-b-[2px]">
+    <article className="md:border-b-foreground flex flex-col-reverse md:grid md:grid-cols-2 md:border-b-[2px]">
       <div className="md:border-r-foreground flex flex-col justify-between py-8 md:border-r-[1px]">
         {/* top */}
         <div className="flex flex-row justify-between">
@@ -43,30 +43,36 @@ export function Project(props: Project) {
           <Link
             href={props.link.href}
             target={props.link.openInNewTab ? "_blank" : "_self"}
-            className="flex flex-col items-end gap-3 p-4 pr-8"
+            className="flex flex-col items-end gap-1 p-4 pr-0 pt-0 md:gap-3 md:pr-8 [&>svg]:h-4 [&>svg]:w-4 md:[&>svg]:h-8 md:[&>svg]:w-8"
           >
             <Arrow />
             <Text
               as="p"
               style="tag"
-              className="font-monument w-[min-content] text-right leading-[1.4em]"
+              className="font-monument w-[min-content] text-right text-[0.4rem] leading-[1.4em] md:text-[0.6rem]"
             >
               GO TO WEBSITE
             </Text>
           </Link>
         </div>
         {/* bottom */}
-        <ul className="grid grid-cols-2 gap-16 pr-8 md:grid-cols-3">
+        <ul className="grid max-w-[80%] grid-cols-2 gap-x-4 gap-y-8 pt-8 md:max-w-full md:grid-cols-3 md:gap-16 md:pr-8 md:pt-0">
           {props.data.map((answer, index) => (
             <li key={index} className={index < 2 ? "" : "md:row-start-2"}>
               <Text
                 as="p"
                 style="tag"
-                className="text-secondary-foreground font-monument leading-[1.4em] opacity-75"
+                className={
+                  "text-secondary-foreground font-monument text-[0.4rem] leading-[1.4em] opacity-75 md:text-[0.6rem]"
+                }
               >
                 {answer.label}
               </Text>
-              <Text as="p" style="display-4" className="pt-4 [&>*]:block">
+              <Text
+                as="p"
+                style="display-4"
+                className="pt-1 md:pt-4 [&>*]:block"
+              >
                 {answer.answers.map((text, index) => (
                   <span key={index}>{text}</span>
                 ))}
@@ -87,9 +93,8 @@ export function Project(props: Project) {
           sizes="(min-width: 1024px) 50vw, 100vw"
           style={{
             aspectRatio: `${props.image.width} / ${props.image.height}`,
-            height: "750px",
           }}
-          className="md:border-l-foreground object-cover md:border-l-[1px]"
+          className="md:border-l-foreground h-[15rem] object-cover md:h-[750px] md:border-l-[1px]"
         />
       </Link>
     </article>
